be2b1dcfca7e2eb510a4571433eacd32e889733b
Chang Liu <cliu18@ncsu.edu>
2016-02-24 01:44:13 -0500

    done

--- chang/hw1-proc/test_func.m
+++ chang/hw1-proc/test_func.m
@@ -45,4 +45,42 @@ for i = 1:length(listFunc)
     close(fig);
 end
 
+% increase the size of knots(5 times)
+x = 0:0.02:1;
+N = 100;
+for i = 1:length(listFunc)
+    f = listFunc{i};
+    y_true = f(x);
+    [yy,xx,errmax] = S_nat(f,x,N);
+    yy_buildin = spline(x,y_true,xx);
+    yy_true = f(xx);
+    % print the error pre block
+    fprintf('\nFunction: %s\n',nameFunc{i});
+    fprintf('ERR for each block\n');
+    fprintf('block \t\t errmax\n');
+    for j = 1:length(errmax)
+        fprintf('%.2f,%.2f\t%6E\n',x(j),x(j+1),errmax(j));
+    end
+    
+    % plot out
+    fig = figure;
+    hold on
+    plot(x,y_true,'o')
+    plot(xx,yy_true,'g')
+    plot(xx,yy,'b')
+    plot(xx,yy_buildin,'r')    
+    legend('knots','True value','My Own script','MATLAB build-in')
+    title(nameFunc{i},'Interpreter','latex','fontsize',24)
+    xlabel('x')
+    ylabel('f(x)')
+    fname = sprintf('funcL_%d',i);
+    savefig(fname);
+    print(fig,fname,'-depsc','-tiff');
+    print(fig,fname,'-dpng');
+    close(fig);
+end
+
+
+
+
 diary off
